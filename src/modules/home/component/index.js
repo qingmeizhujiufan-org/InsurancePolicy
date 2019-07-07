bfb6ea25ad3edcadda9d75350ba0955330c12764
weiwang <2424947384@qq.com>
2019-07-08 01:17:53 +0800

    修改排名头像

--- src/modules/home/component/index.js
+++ src/modules/home/component/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {List, Flex, WingBlank, WhiteSpace, Icon, Toast, Modal, Radio, Button} from 'antd-mobile';
-import {Layout} from 'zui-mobile';
-import {CardList} from 'Comps';
+import { List, Flex, WingBlank, WhiteSpace, Icon, Toast, Modal, Radio, Button } from 'antd-mobile';
+import { Layout } from 'zui-mobile';
+import { CardList } from 'Comps';
 import '../index.less';
 import DocumentTitle from "react-document-title";
 import axios from 'Utils/axios';
-import avatar from 'Img/default.png';
+import restUrl from "RestUrl";
+import bgImg from 'Img/bg.png';
+import avatar from 'Img/hand-loging.png';
 
 const Item = List.Item;
 const Brief = Item.Brief;
@@ -19,6 +21,7 @@ class Index extends React.Component {
         this.state = {
             userId: sessionStorage.getItem('userId'),
             userInfo: {},
+            firstUser: {},
             params: {
                 pageNumber: 1,
                 pageSize: 10,
@@ -41,7 +44,7 @@ class Index extends React.Component {
     };
 
     componentWillMount() {
-        const {time, condition} = this.state;
+        const { time, condition } = this.state;
         this.querySumOne(time.value, condition.value);
     }
 
@@ -53,7 +56,7 @@ class Index extends React.Component {
     }
 
     showModal = () => {
-        const {time, condition} = this.state;
+        const { time, condition } = this.state;
         this.setState({
             tempTime: time,
             tempCondition: condition,
@@ -62,7 +65,7 @@ class Index extends React.Component {
     }
 
     onClose = () => {
-        const {time, condition} = this.state;
+        const { time, condition } = this.state;
         this.setState({
             modalShow: false
         })
@@ -81,7 +84,7 @@ class Index extends React.Component {
     }
 
     onOk = () => {
-        const {tempTime, tempCondition, params} = this.state;
+        const { tempTime, tempCondition, params } = this.state;
         this.setState({
             time: tempTime,
             condition: tempCondition,
@@ -107,7 +110,7 @@ class Index extends React.Component {
 
     querySumOne = (key1, key2) => {
         Toast.loading('正在加载', 0);
-        const {userId} = this.state;
+        const { userId } = this.state;
         const param = {
             id: '30b13970-0052-11e9-8735-190581b1698c',
             time: key1,
@@ -119,7 +122,9 @@ class Index extends React.Component {
             if (data.success) {
                 const backData = data.backData;
                 this.setState({
-                    userInfo: backData
+                    userInfo: backData.user,
+                    firstUser: backData.firstUser
+
                 });
                 Toast.hide()
             } else {
@@ -131,32 +136,35 @@ class Index extends React.Component {
     }
 
     render() {
-        const {modalShow, time, condition, tempTime, tempCondition, params, userInfo} = this.state;
+        const { modalShow, time, condition, tempTime, tempCondition, params, userInfo, firstUser } = this.state;
+        const bgFile = firstUser.bgFile;
+
+        console.log(firstUser)
 
         const times = [
-            {value: 0, label: '月度排行'},
-            {value: 1, label: '季度排行'},
-            {value: 2, label: '年度排行'},
+            { value: 0, label: '月度排行' },
+            { value: 1, label: '季度排行' },
+            { value: 2, label: '年度排行' },
         ];
         const conditions = [
-            {value: 0, label: '累计保费'},
-            {value: 1, label: '保单数量'},
+            { value: 0, label: '累计保费' },
+            { value: 1, label: '保单数量' },
         ];
-        const CutModal = ({className = '', data, ...restProps}) => (
+        const CutModal = ({ className = '', data, ...restProps }) => (
             <div className="condition-container">
                 <div className="condition-title">时间段</div>
                 <div className="condition-item">
                     {times.map(i => {
-                            let type = tempTime.value === i.value ? 'primary' : 'default';
-                            return (
-                                <Button
-                                    key={i.value}
-                                    type={type}
-                                    size='small'
-                                    onClick={() => this.onChange1(i)}>
-                                    {i.label}
-                                </Button>)
-                        }
+                        let type = tempTime.value === i.value ? 'primary' : 'default';
+                        return (
+                            <Button
+                                key={i.value}
+                                type={type}
+                                size='small'
+                                onClick={() => this.onChange1(i)}>
+                                {i.label}
+                            </Button>)
+                    }
                     )}
                 </div>
                 <div className="condition-title">显示条件</div>
@@ -174,28 +182,33 @@ class Index extends React.Component {
                     })
                     }
                 </div>
-                <WhiteSpace size="lg"/>
+                <WhiteSpace size="lg" />
                 <div className="condition-btn" onClick={() => {
                     this.onOk()
                 }}>
-                    <Icon type="check"/>确认
+                    <Icon type="check" />确认
                 </div>
                 <div className="condition-other-btn" onClick={() => {
                     this.toOuterUrl()
                 }}>
-                    <Button size="small" className="green-blue-btn" style={{marginRight: '10px'}}>产品销量榜<Icon
-                        type="right"/></Button>
-                    <Button size="small" className="green-ghost-btn">公司偿付榜<Icon type="right"/></Button>
+                    <Button size="small" className="green-blue-btn" style={{ marginRight: '10px' }}>产品销量榜<Icon
+                        type="right" /></Button>
+                    <Button size="small" className="green-ghost-btn">公司偿付榜<Icon type="right" /></Button>
                 </div>
             </div>
         );
 
-        const SortItem = ({className = '', data = {}, ...restProps}) => (
+        const SortItem = ({ className = '', data = {}, ...restProps }) => (
             <div className={`${className} list-item`} {...restProps}>
                 <div className="sort-item-left">
                     <div className="item-sort-num">{data.index}</div>
                     <div className="item-src">
-                        <img src={avatar}/>
+                        {
+                            data.headimgurl
+                                ? <img src={restUrl.FILE_ASSET + data.headimgurl + data.fileType} />
+                                : <img src={avatar} />
+                        }
+
                     </div>
                     <div className="item-info">
                         <div>{data.realname}</div>
@@ -219,7 +232,13 @@ class Index extends React.Component {
 
         const row = (rowData, sectionID, rowID) => {
             const obj = rowData;
+
             obj.index = parseInt(rowID) + 1;
+            // if (rowID == 0) {
+            //     this.setState({
+            //         firstUser: obj
+            //     })
+            // }
 
             return (
                 <SortItem key={rowID} className="user-sum-item" data={obj}></SortItem>
@@ -231,31 +250,41 @@ class Index extends React.Component {
                 <Layout className={`home${modalShow ? ' mask' : ''}`}>
                     <Layout.Content>
                         <div className="user-info-container">
-                            <WingBlank>
-                                <Flex justify="center" className="user-info-detail">
-                                    <div className="user-logo">
-                                        <img src={avatar} alt=""/>
-                                    </div>
-                                    <div className="user-name">王玮获得了第1名</div>
-                                    <div className="user-operation">
-                                        <div className="user-btn" onClick={() => {
-                                            this.onAddOrder()
-                                        }}>新增订单
+                            <div className="user-bg">
+                                {
+                                    bgFile && bgFile.id
+                                        ? <img className='user-bg' src={restUrl.FILE_ASSET + bgFile.id + bgFile.fileType} alt="" />
+                                        : <img className='user-bg' src={bgImg} alt="" />
+                                }
+
+                            </div>
+                            <Flex justify="center" className="user-info-detail">
+                                <div className="user-logo">
+                                    {
+                                        firstUser && firstUser.headimgurl
+                                            ? <img className='user-bg' src={restUrl.FILE_ASSET + firstUser.headimgurl + firstUser.fileType} alt="" />
+                                            : <img className='user-bg' src={avatar} alt="" />
+                                    }
+                                </div>
+                                <div className="user-name">{firstUser.realname} &nbsp;获得了第1名</div>
+                                <div className="user-operation">
+                                    <div className="user-btn" onClick={() => {
+                                        this.onAddOrder()
+                                    }}>新增订单
                                         </div>
-                                        <div className="user-btn" onClick={() => {
-                                            this.showModal()
-                                        }}>
-                                            <div>{time.label}</div>
-                                            <div>—</div>
-                                            <div>{condition.label}</div>
-                                        </div>
+                                    <div className="user-btn" onClick={() => {
+                                        this.showModal()
+                                    }}>
+                                        <div>{time.label}</div>
+                                        <div>—</div>
+                                        <div>{condition.label}</div>
                                     </div>
-                                </Flex>
-                            </WingBlank>
+                                </div>
+                            </Flex>
                         </div>
                         <SortItem className="user-sum-item" data={userInfo} onClick={() => {
                             this.toUserCenter()
-                        }}/>
+                        }} />
                         <CardList
                             className="user-sum-list"
                             pageUrl={'user/querySumList'}
@@ -269,9 +298,9 @@ class Index extends React.Component {
                             transparent
                             maskClosable={true}
                             onClose={() => this.onClose()}
-                            wrapProps={{onTouchStart: this.onWrapTouchStart}}
+                            wrapProps={{ onTouchStart: this.onWrapTouchStart }}
                         >
-                            <CutModal/>
+                            <CutModal />
                         </Modal>
 
                     </Layout.Content>
