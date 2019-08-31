f4c49aea9330f6b76e772e3c49c37be6b0289fe0
zhongzhenga <616028858@qq.com>
2019-08-31 20:11:51 +0800

    新增订单字段；

--- src/actions/RestUrl.js
+++ src/actions/RestUrl.js
@@ -1,13 +1,13 @@
 /* 本地环境 */
-const ADDR = 'http://192.168.0.102:7001';
+// const ADDR = 'http://localhost:7001';
 /* 开发环境 */
 // const ADDR = 'http://www.xuecheh.com';
 /* 生产环境 */
-// const ADDR = 'http://wujie.hbfolkways.com';
+const ADDR = 'http://www.baolh.top';
 
 module.exports = {
     ADDR,
     BASE_HOST: ADDR + '/api/',
-    FILE_UPLOAD_HOST: 'http://www.xuecheh.com/api/',
-    FILE_ASSET: 'http://www.xuecheh.com/public/',
+    FILE_UPLOAD_HOST: 'http://www.baolh.top/api/',
+    FILE_ASSET: 'http://www.baolh.top/public/',
 };
